72c59bc148d0d095305ea9cca2201b8cf1b19763
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-02-11 21:34:12 -0300

    [SOL] Remove expand in order option (#135)

--- llvm/lib/Target/SBF/SBFISelLowering.h
+++ llvm/lib/Target/SBF/SBFISelLowering.h
@@ -28,7 +28,6 @@ enum NodeType : unsigned {
   SELECT_CC,
   BR_CC,
   Wrapper,
-  MEMCPY,
 };
 }
 
@@ -161,10 +160,6 @@ private:
 
   unsigned EmitSubregExt(MachineInstr &MI, MachineBasicBlock *BB, unsigned Reg,
                          bool isSigned) const;
-
-  MachineBasicBlock * EmitInstrWithCustomInserterMemcpy(MachineInstr &MI,
-                                                        MachineBasicBlock *BB)
-                                                        const;
 
 };
 }
