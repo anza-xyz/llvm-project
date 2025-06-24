4db6645824f8658de401355660732e209aa3539b
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-06-24 12:35:04 -0300

    [SOL] Revert symbol tab hacks to have local symbols in the dynamic symbol table (#157)
    * Revert "[SOL] Refactor iSbfV3 into isSbfV3orHigher (#151)"
    
    This reverts commit 41daae480acb3e62c5f6c7e1c8d4098077d60d09.
    
    * Revert "[SOL] Add local symbols to dynamic symbol table (#130)"
    
    This reverts commit de1b5ebb9ff3b6407f61e393780d01961d12e003.

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -320,10 +320,6 @@ static OutputSection *findSection(StringRef name, unsigned partition = 1) {
   return nullptr;
 }
 
-static bool isSbfV3OrHigher() {
-  return config->emachine == EM_SBF && config->eflags >= 0x3;
-}
-
 template <class ELFT> void elf::createSyntheticSections() {
   // Initialize all pointers with NULL. This is needed because
   // you can call lld::elf::main more than once as a library.
@@ -777,11 +773,6 @@ static void demoteAndCopyLocalSymbols() {
         demoteDefined(*dr, sectionIndexMap);
       else if (in.symTab && includeInSymtab(*b) && shouldKeepInSymtab(*dr))
         in.symTab->addSymbol(b);
-
-      // We want local functions in the dynamic symbol table for SBFv3
-      if (isSbfV3OrHigher() && includeInSymtab(*b) && shouldKeepInSymtab(*dr) &&
-          b->type == STT_FUNC)
-        partitions[b->partition - 1].dynSymTab->addSymbol(b);
     }
   }
 }
@@ -1701,7 +1692,7 @@ template <class ELFT> void Writer<ELFT>::finalizeAddressDependentContent() {
   AArch64Err843419Patcher a64p;
   ARMErr657417Patcher a32p;
   script->assignAddresses();
-  bool SbfDuplicateRemoval = false;
+
   // .ARM.exidx and SHF_LINK_ORDER do not require precise addresses, but they
   // do require the relative addresses of OutputSections because linker scripts
   // can assign Virtual Addresses to OutputSections that are not monotonically
@@ -1752,16 +1743,6 @@ template <class ELFT> void Writer<ELFT>::finalizeAddressDependentContent() {
     }
 
     const Defined *changedSym = script->assignAddresses();
-
-    if (!SbfDuplicateRemoval && isSbfV3OrHigher()) {
-      // When we deduplicate symbols, the address dependent content must be
-      // recalculated as the symbol table might have been shortened.
-      for (Partition &part: partitions) {
-        part.dynSymTab->sortAndDedupSymbolsByValue();
-      }
-      SbfDuplicateRemoval = true;
-      changed = true;
-    }
 
     if (!changed) {
       // Some symbols may be dependent on section addresses. When we break the
@@ -2094,13 +2075,6 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
         if (auto *file = dyn_cast_or_null<SharedFile>(sym->file))
           if (file->isNeeded && !sym->isUndefined())
             addVerneed(sym);
-      } else if (isSbfV3OrHigher() && includeInSymtab(*sym) && isa<Defined>(sym) &&
-                 sym->type == STT_FUNC) {
-        // We want local functions in the dynamic symbol table for SBFv3
-        const Defined *Def = dyn_cast<Defined>(sym);
-        if (shouldKeepInSymtab(*Def)) {
-          partitions[sym->partition - 1].dynSymTab->addSymbol(sym);
-        }
       }
     }
 
