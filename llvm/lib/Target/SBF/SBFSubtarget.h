1449382f736f1f15c1f2a49f870134fc283a2ee0
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-11-05 11:32:01 -0300

    [SOL] Change `callx` encoding for sBPFv3 (#175)

--- llvm/lib/Target/SBF/SBFSubtarget.h
+++ llvm/lib/Target/SBF/SBFSubtarget.h
@@ -76,6 +76,9 @@ protected:
   // Whether to encode destination register in Callx's src field
   bool CallxRegSrc;
 
+  // Whether to encode destination register in Callx's dst field
+  bool CallxRegDst;
+
   // Whether we have the PQR instruction class
   bool HasPqrClass;
 
@@ -116,6 +119,7 @@ public:
   bool getReverseSubImm() const { return ReverseSubImm; }
   bool getNoLddw() const { return NoLddw; }
   bool getCallXRegSrc() const { return CallxRegSrc; }
+  bool getCallXRegDst() const { return CallxRegDst; }
   bool getHasPqrClass() const { return HasPqrClass; }
   bool getHasStoreImm() const { return HasStoreImm; }
   bool getHasExplicitSignExt() const { return HasExplicitSignExt; }
