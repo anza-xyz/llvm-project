c9974c634577bab1736b40ee02d1c5664c3bc1d2
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-11-24 19:00:58 -0300

    [SOL] Invert indexes for dynamic stack frames in v3 (#178)
    * Invert indexes for dynamic stack frames in v3
    
    * Fix rebase conflicts

--- llvm/lib/Target/SBF/SBFSubtarget.cpp
+++ llvm/lib/Target/SBF/SBFSubtarget.cpp
@@ -58,6 +58,7 @@ void SBFSubtarget::initializeEnvironment(const Triple &TT) {
   NewMemEncoding = false;
   HasStaticSyscalls = false;
   IsAbiV2 = false;
+  HasDynamicFramesV3 = false;
 }
 
 void SBFSubtarget::initSubtargetFeatures(StringRef CPU, StringRef FS) {
--- llvm/lib/Target/SBF/SBFSubtarget.h
+++ llvm/lib/Target/SBF/SBFSubtarget.h
@@ -94,6 +94,9 @@ protected:
   // Whether we are using AbiV2
   bool IsAbiV2;
 
+  // Whether we are dealing with dynamic stack frames in SBPFv3
+  bool HasDynamicFramesV3;
+
   std::unique_ptr<CallLowering> CallLoweringInfo;
   std::unique_ptr<InstructionSelector> InstSelector;
   std::unique_ptr<LegalizerInfo> Legalizer;
@@ -113,7 +116,12 @@ public:
   void ParseSubtargetFeatures(StringRef CPU, StringRef TuneCPU, StringRef FS);
   bool getHasJmpExt() const { return HasJmpExt; }
   bool getHasAlu32() const { return HasAlu32; }
-  bool getHasDynamicFrames() const { return HasDynamicFrames; }
+  bool getHasDynamicFrames() const {
+    return HasDynamicFrames || HasDynamicFramesV3;
+  }
+  bool isDynamicFramesV1() const {
+    return HasDynamicFrames;
+  }
   bool getUseDwarfRIS() const { return UseDwarfRIS; }
   bool getDisableNeg() const { return DisableNeg; }
   bool getReverseSubImm() const { return ReverseSubImm; }
