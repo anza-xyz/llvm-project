3e46e32edfe4b4054d53f50938b2957240587240
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2026-07-31 11:56:06 -0300

    [SOL] Read addends for `R_SBF_64_ABS64` relocations (#199)
    * Fix reloc
    
    * Add a test

--- lld/test/lit.cfg.py
+++ lld/test/lit.cfg.py
@@ -82,6 +82,7 @@ llvm_config.feature_config(
                 "MSP430": "msp430",
                 "PowerPC": "ppc",
                 "RISCV": "riscv",
+                "SBF": "sbf",
                 "Sparc": "sparc",
                 "WebAssembly": "wasm",
                 "X86": "x86",
