4752cb00b7e84b7bb5b67f5644fc15d7ce96ed5b
Lucas <lucas.tnagel@gmail.com>
2026-08-11 12:07:41 -0300

    Fix compilation errors

--- lld/ELF/Target.cpp
+++ lld/ELF/Target.cpp
@@ -60,8 +60,6 @@ TargetInfo *elf::getTarget() {
     return getARMTargetInfo();
   case EM_AVR:
     return getAVRTargetInfo();
-  case EM_BPF:
-    return getBPFTargetInfo();
   case EM_SBF:
   case EM_BPF:
     return getSBFTargetInfo();
